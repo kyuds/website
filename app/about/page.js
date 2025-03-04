445cdf9088108e15c218358d1e9e6ee7df85f582
kyuds <kyuseung1016@gmail.com>
2025-03-04 17:10:47 +0900

    fix amplify issue

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ export default function About() {
         <div className="text-foreground w-[85%] md:w-2/5 mt-4 text-center leading-relaxed p-5 flex flex-col items-center font-[family-name:var(--font-geist-mono)]">
             <p className="text-l font-bold ">Hi!</p>
             <p className="mt-5">
-                I'm <b>Daniel Shin (신규승)</b>, a software engineer, boba aficonado, and aspiring entrepreneur. 
+                My name is <b>Daniel Shin (신규승)</b>, a software engineer, boba aficonado, and aspiring entrepreneur. 
                 I am currently a student at UC Berkeley, majoring in Electrical Engineering and Computer Sciences. 
                 Right now, I am taking some gap years, working at <b>Everspin</b>, a cybersecurity firm back in my 
                 home country, South Korea. My personal interests in computer science span <b>distributed systems</b>  
